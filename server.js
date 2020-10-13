34f33baeb239e4ed836fa6b49016c159e7877e86
James Lamb <jameslamb13@gmail.com>
2020-10-13 13:50:32 -0400

    fixed jquery positioning.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("/public"));
+app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
