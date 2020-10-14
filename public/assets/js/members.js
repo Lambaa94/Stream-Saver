0b8598d56d24503703480bcda4eb1755478d49df
Max Goldstein <magoldstein93@gmail.com>
2020-10-14 15:24:04 -0400

    post route

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/watchlists/" + 1).then(function(data) {
-   console.log(data);
+  // and updates the HTML with the users movie list
+  $.get("/api/watchlists/").then(function (data) {
+    console.log(data);
+  });
+  // Put route to add new movie/show to the database
+  $.post("api/watchlists/", function (data) {
+    console.log(data)
   });
 });
