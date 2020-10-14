5619b567e9375d548b6f74b4c4f14aa48ee8fa1b
Max Goldstein <magoldstein93@gmail.com>
2020-10-14 13:21:25 -0400

    watchlistroutes

---
+++ routes/watchlistroutes.js
@@ -0,0 +1,59 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the posts
+  app.get("/api/watchlists", function(req, res) {
+    
+    db.watchList.findAll({
+        include: [db.Users]
+    //   where: req.params.UserId
+    }).then(function(data) {
+    console.log(data, "LOOK HERE")
+      res.json(data)
+     
+    });
+  });
+
+
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    db.Post.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/posts/:id", function(req, res) {
+    db.Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+};
--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(passport.session());
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+require("./routes/watchlistroutes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
