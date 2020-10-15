5a7c18b52bf064e911cc29e04c6ab5f24005a208
Ian <ianwfuzzy@gmail.com>
2020-10-15 13:46:33 -0400

    updating header

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
-<<<<<<< HEAD
   // and updates the HTML with the users movie list
   $.get("/watchlists").then(function (data) {
     
@@ -10,12 +9,4 @@ $(document).ready(function () {
   $.post("api/watchlists/", function (data) {
     console.log(data)
   });
-=======
-  // and updates the HTML on the page
-  
-  // $.get("/api/watchlist").then(function(data){
-  //   console.log(data)
-  //   // $(".streams").text(data.title)
-  // })
->>>>>>> 027f27624406c521d6b57e7f8dc8ce339aee7254
 });
